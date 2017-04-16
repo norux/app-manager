b9e2e9a813f5a8fada9bd1c6c46c461f4e0b2fdb
Andrew Heebum Kwak <andrew@norux.me>
2017-04-16 22:02:09 +0900

    초기 구조 잡기
    Quick start는 README.md  참고

---
+++ src/core/main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+
+(function main() {
+    let mainWindow;
+
+    var createWindow = function() {
+        mainWindow = new BrowserWindow({
+            width: 800,
+            height: 600
+        });
+
+        console.log(`${__dirname}/../viewer/index.html`);
+        mainWindow.loadURL(`file://${__dirname}/../viewer/index.html`);
+
+        if(process.argv[2] && process.argv[2].toString().toLowerCase().trim() == 'dev') {
+            mainWindow.webContents.openDevTools();
+        }
+
+        mainWindow.on('closed', function () {
+            mainWindow = null;
+        });
+
+        require('./menu').createMenu();
+    };
+
+    app.on('ready', createWindow);
+
+    app.on('window-all-closed', function () {
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    });
+
+    app.on('activate', function () {
+        if (mainWindow === null) {
+            createWindow();
+        }
+    });
+})();
